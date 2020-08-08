90c3c6a7f2d3845173eac118342be5f22f811618
raghul adhitya <raghuladhitya2013@gmail.com>
2020-08-08 23:21:08 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+	        <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+	            <span className="navbar-toggler-icon"></span>
+	        </button>
+		    <span className="navbar-brand title heading">VECode</span>
+	        <div className="navbar-nav ml-auto items" id="navbarCollapse">
+	            <div className="navbar-nav">
+	                <ul className="nav navbar-nav">
+			            <li className="nav-item">
+			                <a href="index.html" className="nav-link">Home</a>
+			            </li>		               
+			            <li className="nav-item">
+			                <a href="#" className="nav-link">Profile</a>			                 
+			            </li>			              
+			            <li className="nav-item">
+			                <a href="#" className="nav-link">MCQ</a>
+			            </li>
+                        <li className="nav-item">
+			                <a href="#" className="nav-link">Info</a>
+			            </li>
+                        <li className="nav-item">
+			                <a href="#" className="nav-link">About Us</a>
+			            </li>
+			        </ul>
+	            </div>
+	        </div>
+	    </nav>
+    );
+}
+
+export default Header;
