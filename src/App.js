998223e1e67290d9180aa19ba75533130a73d438
raghul adhitya <raghuladhitya2013@gmail.com>
2020-08-18 10:27:21 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -8,77 +8,82 @@ import Welcome from './components/Welcome';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Register from './components/Register';
+import Aboutus from './components/Aboutus';
 
 function App() {
-  const [welcome, setWelcome] = useState(true);
-  const [login, setLogin] = useState(false);
-  const [signup, setSignup] = useState(false);
-  const [register, setRegister] = useState(false);
-  const [home, setHome] = useState(false);
-  const [uname, setUname] = useState("");
-  const [rnumber, setRnumber] = useState("");
-  const [cpass, setCpass] = useState("");
+//   const [welcome, setWelcome] = useState(true);
+//   const [login, setLogin] = useState(false);
+//   const [signup, setSignup] = useState(false);
+//   const [register, setRegister] = useState(false);
+//   const [home, setHome] = useState(false);
+//   const [uname, setUname] = useState("");
+//   const [rnumber, setRnumber] = useState("");
+//   const [cpass, setCpass] = useState("");
 
-  function onWelcome () {
-    setWelcome(false)
-    setLogin(true)
-  }
+//   function onWelcome () {
+//     setWelcome(false)
+//     setLogin(true)
+//   }
 
-  function onSignup () {
-    setLogin(false)
-    setSignup(true)
-  }
+//   function onSignup () {
+//     setLogin(false)
+//     setSignup(true)
+//   }
 
-  function onSignin () {
-    setSignup(false)
-    setLogin(true)
-  }
+//   function onSignin () {
+//     setSignup(false)
+//     setLogin(true)
+//   }
 
-  function onVerify(name, number, pass) {
-    setUname(name);
-    setRnumber(number);
-    setCpass(pass);
-    setRegister(true);
-    setSignup(false);
-  }
+//   function onVerify(name, number, pass) {
+//     setUname(name);
+//     setRnumber(number);
+//     setCpass(pass);
+//     setRegister(true);
+//     setSignup(false);
+//   }
 
-  function onSignIn() {
-    setRegister(false);
-    setLogin(true);
-  }
+//   function onSignIn() {
+//     setRegister(false);
+//     setLogin(true);
+//   }
 
-  function Home() {
-    setRegister(false);
-    setHome(true);
-  }
+//   function Home() {
+//     setRegister(false);
+//     setHome(true);
+//   }
 
-  function HomeScreen() {
-    setHome(true);
-    setLogin(false);
-  }
+//   function HomeScreen() {
+//     setHome(true);
+//     setLogin(false);
+//   }
 
-  if (welcome) {
-    return <Welcome onWelcome={() => onWelcome()} />
-  } 
-  if (login) {
-    return <Login onSignup={() => onSignup()} HomeScreen={() => HomeScreen()} />
-  }
-  if (signup) {
-    return <Signup onSignin={() => onSignin()} onVerify={onVerify} />
-  }
-  if (register) {
-    return <Register onSignin={() => onSignIn()} name={uname} number={rnumber} pass={cpass} Home={() => Home()} />
-  }
-  if (home) {
+//   if (welcome) {
+//     return <Welcome onWelcome={() => onWelcome()} />
+//   } 
+//   if (login) {
+//     return <Login onSignup={() => onSignup()} HomeScreen={() => HomeScreen()} />
+//   }
+//   if (signup) {
+//     return <Signup onSignin={() => onSignin()} onVerify={onVerify} />
+//   }
+//   if (register) {
+//     return <Register onSignin={() => onSignIn()} name={uname} number={rnumber} pass={cpass} Home={() => Home()} />
+//   }
+//   if (home) {
+//     return (
+//       <div>
+//         <Header />
+//         <Hackerrank />
+//         <Services />
+//         <Contact />
+//       </div>
+//     );
+//   }
+
     return (
-      <div>
-        <Header />
-        <Hackerrank />
-        <Services />
-        <Contact />
-      </div>
+      <Aboutus />
     );
-  }
 }
 
 export default App;
