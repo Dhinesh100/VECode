ed006f4c001da4158a9b6189d03b6db4abd3db37
raghul adhitya <raghuladhitya2013@gmail.com>
2020-08-23 11:00:37 +0530

    responsive layout

--- src/components/InfoTab.js
+++ src/components/InfoTab.js
@@ -12,17 +12,17 @@ const InfoTab = () => {
             <div className="d-flex justify-content-around flex-column align-items-center ml-2 mr-2">
                 <h3 textAlign="center">MEDALS</h3><br />
                 <div className="d-flex justify-content-around">
-                    <img src={gold} style={{width: '3.5em', height: '3.5em'}} />
+                    <img src={gold} style={{width: '3.5em', height: '3.5em'}} alt="Gold Medal" />
                 </div>
                 <p>The Gold Medal has a weightage of 100 points. It will be awarded to the top 3 contestants
                     in events like Codeathons and Hackathons.</p>
                 <div className="d-flex justify-content-around">
-                    <img variant="top" src={silver} style={{width: '3.5em', height: '3.5em'}} />
+                    <img variant="top" src={silver} style={{width: '3.5em', height: '3.5em'}} alt="Silver Medal" />
                 </div>
                 <p>The Silver Medal is worth 10 points. It will be presented to students in the top 5 positions
                     in the Weekly Code Challenge.</p>
                 <div className="d-flex justify-content-around">
-                    <img variant="top" src={bronze} style={{width: '3.5em', height: '3.5em'}} />
+                    <img variant="top" src={bronze} style={{width: '3.5em', height: '3.5em'}} alt="Bronze Medal" />
                 </div>
                 <p>The Bronze Medal has a value of 1 point. It will be
                     presented to students solving the daily MCQ.</p>
